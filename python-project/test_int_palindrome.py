13c399b84416953fcf076f0beeeaf215a9e91d84
Julien Grand-Mourcel <julien.grand-mourcel@mfglabs.com>
2024-12-11 15:35:35 +0100

    fix: more test case and fix on str builtin usage

--- python-project/test_int_palindrome.py
+++ python-project/test_int_palindrome.py
@@ -3,11 +3,11 @@ from int_palindrome_v0 import is_int_palindrome_julien
 from tech_io_utils import send_msg, success, fail
 import builtins
 
-sorted_builtin_used = False
+str_builtin_used = False
 
 
 def new_str(x):
-    global sorted_builtin_used
+    global str_builtin_used
     str_builtin_used = True
     return orig_str(x)
 
@@ -27,15 +27,17 @@ def test_is_int_palindrome():
         assert is_int_palindrome(101), "101 is a palindrome"
         assert not is_int_palindrome(1011), "1011 is not a palindrome"
         assert is_int_palindrome(10101), "10101 is a palindrome"
+        assert is_int_palindrome(82328), "82328 is a palindrome"
+        assert not is_int_palindrome(823028), "823028 is not a palindrome"
+        assert is_int_palindrome(97823032879), "97823032879 is not a palindrome"
         number_10001 = get_nb_palindromes(0, 990, is_int_palindrome)
         assert number_10001 == get_nb_palindromes(0, 990, is_int_palindrome_julien), f"{number_10001} != {get_nb_palindromes(0, 990, is_int_palindrome_julien)}"
         success()
+        send_msg("Kudos 🌟", "Great job! 🎉")
 
-        if sorted_builtin_used:
-            send_msg("Kudos 🌟", "Great job! 🎉")
+        if str_builtin_used:
             send_msg("Warning ⚠️", "You used the str builtin. Try to solve the exercise without using it")
         else:
-            send_msg("Kudos 🌟", "Great job! 🎉")
             send_msg("Congratulation 🎉", "You solved the exercise without using the str builtin")
     except AssertionError as e:
         fail()
